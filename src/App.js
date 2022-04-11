a38ca8ecaedea40d2c0c2d866eca480df83e96dd
emarald-star <Oxford@01234>
2022-04-11 12:42:18 -0700

    Update form

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ function App() {
           
           <Form />
         </div>
-
-      
-
-        
       </header>
+      <footer>
+        <div className='footer'>
+          This project is created by <strong>Ranjana Joshi</strong> and is <a href='https://github.com/emarald-star/learnreact.git' target="_blank" rel='noopener noreferrer'>open-sourced on Github</a> and <a href='https://velvety-griffin-5c8e51.netlify.app/'>hosted on Netlify</a>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,24 +5,22 @@ import axios from "axios";
 
 export default function Weather(props){
     
-    let [weather,setweather] = useState({temp: 10, 
-                                        desc: "Cloudy", 
-                                        feels:5, 
-                                        humidity: 50, 
-                                        wind:8 }  );
+    let [weather,setweather] = useState({ready:false}  );
     
     function displayWeather(response){
-    setweather({temp:Math.round(response.data.main.temp), 
+    setweather({ready:true,
+                temp:Math.round(response.data.main.temp), 
                 desc:(response.data.weather[0].description), 
                 feels:Math.round(response.data.main.feels_like),
                 humidity:Math.round(response.data.main.humidity),
                 wind:Math.round((response.data.wind.speed)*3.6)})
     }
     
-    let apiKey = "cec780cc1747a8da887667579c7d76d0";
-    let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city1}&appid=${apiKey}&units=metric`;
-    axios.get(apiurl).then(displayWeather);
-
+        let apiKey = "63f25e89554621908d6cac57d52fdced";
+        let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city1}&appid=${apiKey}&units=metric`;
+        axios.get(apiurl).then(displayWeather);
+    
+    if(weather.ready){
     return(
         <div className="Current-details">
             <h1>{props.city1} 
@@ -50,5 +48,13 @@ export default function Weather(props){
             </div>
         </div>
     )
+    }
+
+    else {
+        
+        return "Loading..."
+    }
+    
+    
 }
 
